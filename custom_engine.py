a47593a7e9970796da1d7eb4b92af894530a728d
Bert Huang <bert@cs.tufts.edu>
2021-05-25 17:18:00 -0400

    Attempted to convert pruning to alpha-beta, but results are the same?

--- custom_engine.py
+++ custom_engine.py
@@ -7,12 +7,12 @@ from math import inf as INFINITY
 from collections import namedtuple
 
 PIECE_VALUES = {
-    'P': 1,
-    'N': 3,
-    'B': 3,
-    'R': 5,
-    'Q': 9,
-    'K': 0,
+    chess.PAWN: 1,
+    chess.KNIGHT: 3,
+    chess.BISHOP: 3,
+    chess.ROOK: 5,
+    chess.QUEEN: 9,
+    chess.KING: 0,
 }
 
 
@@ -24,7 +24,7 @@ def material_count(new_board, turn):
     material_difference = 0
 
     for piece in all_pieces:
-        value = PIECE_VALUES[piece.symbol().upper()]
+        value = PIECE_VALUES[piece.piece_type]
         if piece.color == turn:
             material_difference += value
         else:
@@ -50,7 +50,7 @@ def improved_score(new_board, turn):
         if new_board.is_attacked_by(not turn, square):
             space -= 1
 
-    score += space * 1/32
+    score += space * 1/64
 
     # # remove hanging pieces from material count
     #
@@ -71,10 +71,11 @@ cache_hits = 0
 positions = 0
 
 
-Config = namedtuple("Config", ['prune', 'cache', 'sort', 'max_depth'], defaults=[True, True, False, 3])
+Config = namedtuple("Config", ['prune', 'cache', 'sort', 'max_depth'], defaults=[True, True, True, 4])
 
 
-def minimax_score(board, turn, cutoff=INFINITY, curr_depth=0, cache=(), config=Config()):
+def minimax_score(board, turn, opponent_best=INFINITY, my_best=-INFINITY, curr_depth=0,
+                  cache=(), config=Config()):
 
     global cache_hits, num_pruned, positions
 
@@ -88,31 +89,50 @@ def minimax_score(board, turn, cutoff=INFINITY, curr_depth=0, cache=(), config=C
     moves = list(board.legal_moves)
     best_move = None
     best_score = -INFINITY
+    
+    children = []
 
+    # generate children positions from legal moves
     for move in moves:
         # apply the current candidate move
 
         new_board = board.copy()
         new_board.push(move)
+        
+        sort_score = material_count(new_board, not turn) if config.sort else 0
+
+        children.append((sort_score, new_board, move))
+
+    for _, new_board, move in sorted(children, key=lambda x: x[0], reverse=True):
 
         if config.cache:
-            fen = new_board.fen()
-            if fen not in cache:
-                score = minimax_score(new_board, not turn, -best_score, curr_depth + 1, cache, config)
+            # The cache saves score and depth of score calculation.
 
-                cache[fen] = score
+            fen = new_board.fen()
+            fen = fen[:-4]  # remove move counts from fen
+
+            score, cached_depth = cache[fen] if fen in cache else (0, 0)
+
+            # depth of score estimate if we compute it
+            new_depth = config.max_depth - curr_depth
+
+            # if we could get a deeper estimate than what is in the cache
+            if new_depth > cached_depth:
+                score = minimax_score(new_board, not turn, -my_best, -opponent_best, curr_depth + 1, cache, config)
+
+                cache[fen] = (score, new_depth)
             else:
-                score = cache[fen]
                 cache_hits += 1
         else:
-            score = minimax_score(new_board, not turn, -best_score, curr_depth + 1, cache, config)
+            score = minimax_score(new_board, not turn, -my_best, -opponent_best, curr_depth + 1, cache, config)
 
         if score > best_score:
             best_move = move
             best_score = score
+            my_best = max(best_score, my_best)
 
         if config.prune:
-            if score > cutoff:
+            if score > opponent_best:
                 num_pruned += 1
                 return -best_score
 
@@ -128,6 +148,7 @@ class ScoreEngine(MinimalEngine):
         self.name = name
         self.score_function = minimax_score
         self.config = config
+        self.known_positions = {}
 
     def search(self, board, time_limit, ponder):
         moves = list(board.legal_moves)
@@ -135,15 +156,14 @@ class ScoreEngine(MinimalEngine):
         best_move = None
         best_score = -INFINITY
 
-        known_positions = {}
-
         for move in moves:
             # apply the current candidate move
 
             new_board = board.copy()
             new_board.push(move)
 
-            score = self.score_function(new_board, board.turn, cache=known_positions, config=self.config, curr_depth=1)
+            score = self.score_function(new_board, board.turn, cache=self.known_positions,
+                                        config=self.config, curr_depth=1)
 
             if score > best_score:
                 best_move = move
@@ -153,13 +173,15 @@ class ScoreEngine(MinimalEngine):
 
 
 if __name__ == "__main__":
-    # board = chess.Board('8/5Qpk/B4bnp/8/3r4/PR4PK/1P3P1P/6r1 b - - 2 31')
-    board = chess.Board('3rk3/1p2qp2/2p2n2/1B3bp1/1b1Qp3/8/PPPP1PP1/RNB1K1N1 w Q - 0 23')
+    board = chess.Board('8/5Qpk/B4bnp/8/3r4/PR4PK/1P3P1P/6r1 b - - 2 31')
+    # board = chess.Board('3rk3/1p2qp2/2p2n2/1B3bp1/1b1Qp3/8/PPPP1PP1/RNB1K1N1 w Q - 0 23')
 
-    configs = [Config(prune=False, cache=False),
-               Config(prune=False, cache=True),
-               Config(prune=True, cache=False),
-               Config(prune=True, cache=True),
+    configs = [Config(prune=False, cache=False, sort=False),
+               #Config(prune=False, cache=True, sort=False),
+               # Config(prune=True, cache=False, sort=False),
+               #Config(prune=True, cache=True, sort=False),
+               # Config(prune=True, cache=False, sort=True),
+               #Config(prune=True, cache=True, sort=True),
                ]
 
     for config in configs:
@@ -178,3 +200,5 @@ if __name__ == "__main__":
         print("Cache hits: {}. Prunes: {}. Positions: {}.".format(cache_hits, num_pruned, positions))
 
         print(move)
+
+        print("\n\n\n")
